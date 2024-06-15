1ddb000e61e8c377839cfca0b2ff846a2c298ca2
Danish <dan71ish@gmail.com>
2024-06-15 11:35:25 +0530

    bootstrapped the project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,26 @@
-Bun.serve({
-  fetch(req) {
-    return new Response("Hello from Bun");
-  },
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
 });
+
+app.post("/api/v1/signup", (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  return c.text("Hello there");
+});
+app.post("/api/v1/signin", (c) => {});
+app.post("/api/v1/blog", (c) => {});
+app.put("/api/v1/blog", (c) => {});
+app.get("/api/v1/blog/:id", (c) => {});
+
+export default app;
